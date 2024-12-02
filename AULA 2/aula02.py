c41aedb5a3855aea6edb82386e328a6e85a53d34
brunin577 <brunodogueto47@gmail.com>
2024-12-02 10:00:24 -0300

    Hoje foi aula de variável e

---
+++ AULA 2/aula02.py
@@ -0,0 +1,8 @@
+print('Bom dia a todos')
+
+nome_usuario = 'Bruno Rocha' # tipo -> str = string
+idade_usuario = 19  # tipo -> int = integer
+altura_usuario = 1.68 # tipo -> = float
+maior_de_idade = true  # tipo -> bool = boolean 
+
+print('seja bem-vindo', nome_usuario, ) 'sua idade é', 'sua altura é', 'altura_usuario, 'você é maior de idade?', maior de idade)'
